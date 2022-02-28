5fd182fc8511bfe7bed5d95e9cbc017cdae3ccd4
Sumit Kumar Shakya <sumitshakya20101999@gmail.com>
2022-02-28 15:38:04 +0530

    graph update

--- Graph/Fire Escape Routes_CodeChef.cpp
+++ Graph/Fire Escape Routes_CodeChef.cpp
@@ -1,14 +1,17 @@
 #include <bits/stdc++.h>
+
+#define mod 1000000007
 using namespace std;
 
-void dfs(vector<vector<int>> adj, vector<bool> &vis, int root, int c){
+void dfs(vector<vector<int>> adj, vector<bool> &vis, int root, int &c){
     vis[root]=true;
+    c++;
+
     for(auto ele:adj[root]){
         if(!vis[ele]){
             dfs(adj, vis, ele, c);
         }
     }
-    c++;
 }
 
 int main(){
@@ -17,21 +20,20 @@ int main(){
     while(t--){
         int n,m,u,v;
         cin>>n>>m;
-        vector<vector<int>>adj(n);
+        vector<vector<int>>adj(n+1);
         for(int i=0;i<m;i++){
             cin>>u>>v;
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
         int cc_componenet=0,ans1=1;
-
-        vector<bool>vis(n,false);
+        vector<bool>vis(n+1,false);
         for(int i=1;i<=n;i++){
             int component=0;
             if(!vis[i]){
                 dfs(adj, vis, i, component);
                 cc_componenet++;
-                ans1*=component;
+                ans1=(ans1*component) % mod;
             }
         }
         cout<<cc_componenet<<" "<<ans1<<endl;
